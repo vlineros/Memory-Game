2de7d7dfd76ddd24efb95bc35b47f97623b1991c
vlineros <victorlineros@gmail.com>
2019-07-19 19:38:02 -0500

    Updates

---
+++ precache-manifest.42dfc5a6b0dc9c8c76fe49f00601f68f.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c66b0c50ed18fcb339e3707338288d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3065eb0e9fad5e4846e0",
+    "url": "/static/css/main.02b0f12a.chunk.css"
+  },
+  {
+    "revision": "49d8c68793f81a6e9d74",
+    "url": "/static/js/2.3a5cce55.chunk.js"
+  },
+  {
+    "revision": "3065eb0e9fad5e4846e0",
+    "url": "/static/js/main.abaee84c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "034e3991d2e51885c1605d829ea32619",
+    "url": "/static/media/Billy_STMG.034e3991.jpg"
+  },
+  {
+    "revision": "54c358ee7d09595150408b590b03b647",
+    "url": "/static/media/Bob_STMG.54c358ee.jpg"
+  },
+  {
+    "revision": "af6763bd3189f3bb1296dff2bd4b41ff",
+    "url": "/static/media/Dustin_STMG.af6763bd.jpg"
+  },
+  {
+    "revision": "6233163dae7c74bd62890f37c6fc8e49",
+    "url": "/static/media/Eleven_STMG.6233163d.jpg"
+  },
+  {
+    "revision": "f5b3935a8dafd64289a4edd0da6e094e",
+    "url": "/static/media/Hopper_STMG.f5b3935a.jpg"
+  },
+  {
+    "revision": "d78aa73b2d0bfcc1709e59d1f7ea1f2b",
+    "url": "/static/media/Lucas_STMG.d78aa73b.jpg"
+  },
+  {
+    "revision": "9c90df222ef9690c804191fd01dcf561",
+    "url": "/static/media/Max_STMG.9c90df22.jpg"
+  },
+  {
+    "revision": "a1c71978aaea7ef7f3fb650ffdd2eb98",
+    "url": "/static/media/Murray_STMG.a1c71978.jpg"
+  },
+  {
+    "revision": "44bc92b0e0426c1bcade2341bd1dd8af",
+    "url": "/static/media/Nancy_STMG.44bc92b0.jpeg"
+  },
+  {
+    "revision": "093489800a9c4ca9ca92d19316dcde79",
+    "url": "/static/media/Steve_STMG.09348980.jpg"
+  },
+  {
+    "revision": "459ec91aa07cd10ba1b39bdbd4d47cb4",
+    "url": "/static/media/Will_STMG.459ec91a.jpg"
+  },
+  {
+    "revision": "e46319a2358d720644f7ea34f53bd281",
+    "url": "/static/media/mike_STMG.e46319a2.jpg"
+  }
+]);
